cc6c1dea3ebe0454b63a396ee1468341c1da6d65
pbvmateus <pbvmateus@gmail.com>
2024-06-14 12:41:15 +0100

    test

--- samples/service-contract/helpers.js
+++ samples/service-contract/helpers.js
@@ -54,7 +54,7 @@ https://us.coresuite.com/api/query/v1?&dtos=Equipment.24
             })
               .then(response => response.json())
               .then(function(json) {
-                    resolve(json.data[0]);
+                    resolve(json.data[0].Name);
                 //const serviceContractEquipment = json.data.find(contract => contract.serviceContractEquipment.equipment === activity.equipment);
                 /*if (!serviceContractEquipment) {
                   resolve(null);
