c5e990bfd5193606ace8f2de929259af0126205c
pdbranco <p.d.branco@accenture.com>
2024-06-28 14:24:50 +0100

    Update helpersPolygon.js

--- extensions/polygon/helpersPolygon.js
+++ extensions/polygon/helpersPolygon.js
@@ -11,17 +11,17 @@ function initializeRefreshTokenStrategy(shellSdk, auth) {
 
   shellSdk.on(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, (event) => {
     sessionStorage.setItem('token', event.access_token);
-    setTimeout(() => fetchToken(), (event.expires_in * 1000) - 5000);
+    setTimeout(() => fetchTokenPolygon(), (event.expires_in * 1000) - 5000);
   });
 
-  function fetchToken() {
+  function fetchTokenPolygon() {
     shellSdk.emit(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, {
       response_type: 'token'  // request a user token within the context
     });
   }
 
   sessionStorage.setItem('token', auth.access_token);
-  setTimeout(() => fetchToken(), (auth.expires_in * 1000) - 5000);
+  setTimeout(() => fetchTokenPolygon(), (auth.expires_in * 1000) - 5000);
 }
 
 //GET OBJECT POLYGONS
