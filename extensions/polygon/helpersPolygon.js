54c9685beeb1d0c21af92f161b245f88758c864c
pdbranco <p.d.branco@accenture.com>
2024-07-09 14:32:29 +0100

    Update helpersPolygon.js

--- extensions/polygon/helpersPolygon.js
+++ extensions/polygon/helpersPolygon.js
@@ -315,24 +315,26 @@ async function getIdCustomObject(cloudHost, account, company, nameObject) {
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('myForm');
     const buttonSubmit = document.getElementById('createOrUpdatePolygon');
-    let formChanged = false;
 
-    function checkChange() {
-        formChanged = true;
-        buttonSubmit.disabled = false;
+    if (form && buttonSubmit) {
+	    let formChanged = false;
+	
+	    function checkChange() {
+	        formChanged = true;
+	        buttonSubmit.disabled = false;
+	    }
+	
+	    const fields = form.querySelectorAll('textarea');
+	    fields.forEach(field => {
+	        field.addEventListener('change', checkChange);
+	        field.addEventListener('input', checkChange);
+	    });
+	
+	    form.addEventListener('submit', function() {
+	        formChanged = false;
+	        buttonSubmit.disabled = true;
+	    });
     }
-
-    const fields = form.querySelectorAll('textarea');
-    fields.forEach(field => {
-        field.addEventListener('change', checkChange);
-        field.addEventListener('input', checkChange);
-    });
-
-    form.addEventListener('submit', function() {
-        formChanged = false;
-        buttonSubmit.disabled = true;
-    });
-
 });
 
 function compareByPolygonName(a, b) {
