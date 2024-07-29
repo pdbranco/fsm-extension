9479169f5d063bd76dd86930210cb59230070758
pdbranco <p.d.branco@accenture.com>
2024-07-29 16:40:11 +0100

    Update helpersPWA.js

--- extensions/pwa/helpersPWA.js
+++ extensions/pwa/helpersPWA.js
@@ -109,8 +109,7 @@ function getGroupPolicy(cloudHost, account, company, shellSdk, user) {
             
             fetch(`https://${cloudHost}/api/user/v1/users?account=${account}&company=${company}&name=${user}`, {
                 method: 'GET',
-                headers,
-		body: ''
+                headers
             })
             .then(response => {
                 if (!response.ok) {
