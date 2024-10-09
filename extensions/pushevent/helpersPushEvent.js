bfa609bc8e1acb024bb818bac26e390d46143b5b
pdbranco <p.d.branco@accenture.com>
2024-10-09 09:03:58 +0100

    Update helpersPushEvent.js

--- extensions/pushevent/helpersPushEvent.js
+++ extensions/pushevent/helpersPushEvent.js
@@ -11,7 +11,7 @@ function initializeRefreshTokenStrategy(shellSdk, auth) {
 
     shellSdk.on(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, (event) => {
         sessionStorage.setItem('token', event.access_token);
-        setTimeout(() => fetchToken(), (event.expires_in * 1000) - 5000);
+        setTimeout(() => fetchToken(), (event.expires_in * 500));
     });
 
     function fetchToken() {
